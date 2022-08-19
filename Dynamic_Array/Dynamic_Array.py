67314d27bff8f27791d185fbac5ab65a6e3f7baa
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-19 14:04:20 +0400

    added some methods

--- Dynamic_Array/Dynamic_Array.py
+++ Dynamic_Array/Dynamic_Array.py
@@ -1,4 +1,4 @@
-import ctypes
+import ctypes # this library provides C compatible data types, and allows calling functions in DLLs or shared libraries.
 
 class Dynamic_Array:
     """
@@ -10,6 +10,29 @@ class Dynamic_Array:
         self.n = 0 # actual number of elements (at the creation of d. array it is 0)
         self.capacity  = 1 # Default capacity 
         self.array = self.make_array(self.capacity)
+
+    def __len__(self):
+        """
+        Returns number of elements in D-Array
+        """
+        return self.n
+    
+    def __getitem__(self,k):
+        """
+        Returns element at index K
+        """
+
+        if not 0 <= k < self.n:
+            # check if k is not in bounds of array 
+            return IndexError("Index is out of bounds! ")
+        
+        return self.array[k] # returns an element
+
+    def append(self,element):
+        """
+        Add element to the end of the array
+        """
+        pass
     
     def make_array(self, new_capacity):
         """
