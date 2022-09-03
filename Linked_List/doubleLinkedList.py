c0013fea4e339aae1e834c58c57426ef89f8772a
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-09-03 08:07:20 +0400

    c

--- Linked_List/doubleLinkedList.py
+++ Linked_List/doubleLinkedList.py
@@ -5,10 +5,11 @@ class Node:
         after creation of object, but this is more helpful for single_linked_list
     """
 
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
         self.prev = None
+
 
 class DoubleLinkedList:
     pass
